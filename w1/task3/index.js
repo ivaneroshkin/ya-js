b3c2ed0bd9afa67e5bb6e50056e93a6808e1ee8e
Ivan <ivaneroshk@yandex.ru>
2018-06-12 02:57:27 +0300

    Added solution

--- w1/task3/index.js
+++ w1/task3/index.js
@@ -8,30 +8,18 @@ module.exports = function(hours, minutes, interval) {
   var appendMinutes = 0;
   var appendHours = 0;
 
-  var getOptimalSizeNumber = function(a) {
-    var b = '0';
-    a = b + String(a);
-    return a;
-  };
-
-  if ((interval.lenght = 1)) {
-    getOptimalSizeNumber(interval);
-  }
-
-  // проверка минут
   appendMinutes = minutes + interval;
   if (appendMinutes > 60) {
     appendHours = Math.floor(appendMinutes / 60);
     minutes = appendMinutes % 60;
     hours = hours + appendHours;
-  } else if ((appendMinutes = 60)) {
+  } else if (appendMinutes == 60) {
     hours = hours + 1;
     minutes = '00';
   } else {
     minutes = appendMinutes;
   }
 
-  // проверка часов
   if (hours >= 24) {
     hours = hours % 24;
     if (hours == 0) {
@@ -39,16 +27,18 @@ module.exports = function(hours, minutes, interval) {
     }
   }
 
-  if ((minutes.lenght = 1)) {
-    getOptimalSizeNumber(minutes);
-  }
   if ((hours.lenght = 1)) {
-    getOptimalSizeNumber(hours);
+    hours = '0' + hours;
+    hours.substring(hours.length - 2, hours.length);
+  }
+
+  if ((minutes.lenght = 1)) {
+    minutes = '0' + minutes;
   }
 
   var solutionArray = [];
-  solutionArray.push(hours);
+  solutionArray.push(hours.substring(hours.length - 2, hours.length));
   solutionArray.push(':');
-  solutionArray.push(minutes);
+  solutionArray.push(minutes.substring(minutes.length - 2, minutes.length));
   return String(solutionArray.join(''));
 };
