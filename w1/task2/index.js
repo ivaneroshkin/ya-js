9c38fc7b8aa861f04773f4a1b011539ffc10383d
Ivan <ivaneroshk@yandex.ru>
2018-06-11 22:27:49 +0300

    Added task2 solution

--- w1/task2/index.js
+++ w1/task2/index.js
@@ -3,6 +3,16 @@
  * @param {Number} minutes
  * @returns {Boolean}
  */
-module.exports = function (hours, minutes) {
+module.exports = function(hours, minutes) {
+  var validityHours;
+  var validityMinutes;
 
+  validityHours = hours >= 0 && hours <= 23 ? true : false;
+  validityMinutes = minutes >= 0 && minutes <= 59 ? true : false;
+
+  if (validityHours && validityMinutes) {
+    return true;
+  } else {
+    return false;
+  }
 };
