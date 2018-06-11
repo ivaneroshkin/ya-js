d12a4c16b7deb33f4f4f38fb899f9762e66e44f4
Ivan <ivaneroshk@yandex.ru>
2018-06-11 13:38:53 +0300

    Added w1

---
+++ w1/task1/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Number} a Первое слагаемое
+ * @param {Number} b Второе слагаемое
+ * @returns {Number}
+ */
+module.exports = function(a, b) {
+  return Number(a) + Number(b);
+};
+
+console.log('test');
