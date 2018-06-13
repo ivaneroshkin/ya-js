6dcf551720b099618d66601b245db27455c637d8
Ivan <ivaneroshk@yandex.ru>
2018-06-13 10:59:07 +0300

    Get solution

--- w2/task1/index.js
+++ w2/task1/index.js
@@ -2,6 +2,17 @@
  * @param {String} tweet
  * @returns {String[]}
  */
-module.exports = function (tweet) {
+module.exports = function(tweet) {
+  var resultArray = [];
+  var filterString = function(str) {
+    if (str.indexOf('#') !== -1) {
+      str = str.substr(1, str.length);
+      resultArray.push(str);
+    }
+  };
 
+  var hashTagsArray = [];
+  hashTagsArray = tweet.split(' ');
+  hashTagsArray.forEach(filterString);
+  return resultArray;
 };
