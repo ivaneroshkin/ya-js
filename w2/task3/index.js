21c9f4b40599c441ec160027ca8de3b491bfa515
Ivan <ivaneroshk@yandex.ru>
2018-06-19 23:47:13 +0300

    Set commands

--- w2/task3/index.js
+++ w2/task3/index.js
@@ -6,17 +6,98 @@ var phoneBook = {};
  * @returns {*} - результат зависит от команды
  */
 module.exports = function(command) {
-  // ...
-  // var commandName = ... ; возможно проверка на первое слово в строке
-  // ...
+  var words = command.split(' ');
+  var commandName = words[0];
 
   // Обработка команды ADD
   if (commandName === 'ADD') {
-    // ...
-
-    // Не забыть вернуть результат выполнения функции addContact
-    return addContact(/**/);
+    var addContact = function(string) {
+      // генерация имени
+      var nameContact = words[1];
+      var newNameContact = nameContact + ':';
+      // генерация номеров
+      var numbersContact = words.slice(2);
+      var newNumbersContact = numbersContact.join(' ');
+      var contactString = newNameContact + ' ' + newNumbersContact;
+      // проверка массива
+      if (Array.isArray(phoneBook.contacts)) {
+        // добавление телефона к старому контакту или добавление нового
+        let counter = 0;
+        phoneBook.contacts.forEach(str => {
+          var firstWord = str.substr(0, str.indexOf(' '));
+          if (firstWord === newNameContact) {
+            var oldContact = str + ', ' + newNumbersContact;
+            var indexStr = phoneBook.contacts.indexOf(str);
+            phoneBook.contacts.splice(indexStr, 1);
+            phoneBook.contacts.push(oldContact);
+            phoneBook.contacts.sort();
+          } else {
+            ++counter;
+          }
+          if (counter === phoneBook.contacts.length) {
+            phoneBook.contacts.push(contactString);
+            phoneBook.contacts.sort();
+          }
+        });
+      } else {
+        phoneBook.contacts = [];
+        phoneBook.contacts.push(contactString);
+        phoneBook.contacts.sort();
+      }
+    };
+    return addContact(command);
   }
 
-  // ...Обработка других команд...
+  // обработка команды 'REMOVE_PHONE'
+  if (commandName === 'REMOVE_PHONE') {
+    var removeContact = function(params) {
+      // получить номер и массив для сортировки
+      phoneBook.filtered = [];
+      var deletePhone = words[1];
+      phoneBook.contacts.forEach(element => {
+        var newArray = element.split(' ');
+        // var deleteNumber = '666';
+        // var newStr = 'ivan: 555, 666, 668';
+        // var newA = newStr.split(' ');
+        var findingName = newArray.slice(0, 1);
+        var findingArray = newArray.slice(1);
+        var filteredArray = [];
+        for (let k = 0; k < findingArray.length; k++) {
+          const element = findingArray[k];
+          var replacedElement = element.replace(',', '');
+          if (replacedElement === deletePhone) {
+          } else {
+            filteredArray.push(replacedElement);
+          }
+        }
+        var resultString;
+        if (filteredArray.length > 1) {
+          resultString = filteredArray.join(', ');
+        } else if (filteredArray.length === 1) {
+          resultString = filteredArray.join();
+        } else {
+          resultString = '';
+        }
+        // проверка длины массива
+        if (resultString === '') {
+          return;
+        } else {
+          var final = findingName + ' ' + resultString;
+          element = final;
+          phoneBook.filtered.push(element);
+        }
+      });
+      phoneBook.contacts = phoneBook.filtered;
+    };
+    removeContact(command);
+    return true;
+  }
+
+  // обработка команды 'SHOW'
+  if (commandName === 'SHOW') {
+    var showPhoneBook = function() {
+      return phoneBook.contacts;
+    };
+    return showPhoneBook();
+  }
 };
