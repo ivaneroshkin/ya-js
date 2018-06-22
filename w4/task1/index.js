fc7ed54a85d3043a5d9ab0968ff46c59eb166d46
Ivan <ivaneroshk@yandex.ru>
2018-06-22 17:09:12 +0300

    Added new task in w4

---
+++ w4/task1/index.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Array} collection
+ * @params {Function[]} – Функции для запроса
+ * @returns {Array}
+ */
+function query(collection) {
+
+}
+
+/**
+ * @params {String[]}
+ */
+function select() {
+
+}
+
+/**
+ * @param {String} property – Свойство для фильтрации
+ * @param {Array} values – Массив разрешённых значений
+ */
+function filterIn(property, values) {
+
+}
+
+module.exports = {
+    query: query,
+    select: select,
+    filterIn: filterIn
+};
