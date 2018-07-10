9aa924221766a49c51900ea9a975d59caed80650
Ivan <ivaneroshk@yandex.ru>
2018-07-10 17:19:59 +0300

    Added w7

---
+++ w7/exercise1/index.js
@@ -0,0 +1,18 @@
+module.exports = Collection;
+
+/**
+ * Конструктор коллекции
+ * @constructor
+ */
+function Collection() {}
+
+
+// Методы коллекции
+Collection.prototype.values = function () {};
+// другие методы
+
+
+/**
+ * Создание коллекции из массива значений
+ */
+Collection.from = function () {};
