cbc6e57ac49f84c8ef6f47a59360be0ae5012ee4
Ivan <ivaneroshk@yandex.ru>
2018-07-12 20:11:04 +0300

    Subscribe to events

--- w5/exercise1/index.js
+++ w5/exercise1/index.js
@@ -1,19 +1,43 @@
+let subscriptions = [];
+
 module.exports = {
-  /**
-   * @param {String} event
-   * @param {Object} subscriber
-   * @param {Function} handler
-   */
-  on: function(event, subscriber, handler) {},
+    /**
+     * @param {String} event
+     * @param {Object} subscriber
+     * @param {Function} handler
+     */
+    on: function (event, subscriber, handler) {
+        subscriptions.push({
+            event: event,
+            subscriber: subscriber,
+            handler: handler
+        });
 
-  /**
-   * @param {String} event
-   * @param {Object} subscriber
-   */
-  off: function(event, subscriber) {},
+        return this;
+    },
 
-  /**
-   * @param {String} event
-   */
-  emit: function(event) {}
+    /**
+     * @param {String} event
+     * @param {Object} subscriber
+     */
+    off: function (event, subscriber) {
+        subscriptions = subscriptions.filter(function (subscription) {
+            return subscription.event !== event || subscription.subscriber !== subscriber;
+        });
+
+        return this;
+    },
+
+    /**
+     * @param {String} event
+     */
+    emit: function (event) {
+        subscriptions.forEach(function (subscription) {
+            if (event === subscription.event) {
+                subscription.handler.call(subscription.subscriber);
+            }
+        });
+
+        return this;
+    }
 };
