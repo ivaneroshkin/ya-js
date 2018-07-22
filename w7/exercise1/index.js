8c7cc30330aadb3de617b72c7063153b48e60d37
Ivan <ivaneroshk@yandex.ru>
2018-07-22 16:33:41 +0300

    Constructor for creating a collection of elements

--- w7/exercise1/index.js
+++ w7/exercise1/index.js
@@ -4,15 +4,60 @@ module.exports = Collection;
  * Конструктор коллекции
  * @constructor
  */
-function Collection() {}
+function Collection(array) {
+  if (array === undefined) {
+    this.collection = [];
+  } else {
+    this.collection = array;
+  }
+  //this.collection = array || [];
+}
 
+let methods = {
+  append: function(element) {
+    if (typeof element === 'object') {
+      for (let i = 0; i < element.collection.length; i++) {
+        this.collection.push(element.collection[i]);
+      }
+    } else {
+      this.collection.push(element);
+    }
+  },
 
-// Методы коллекции
-Collection.prototype.values = function () {};
-// другие методы
+  values: function() {
+    return this.collection;
+  },
 
+  count: function() {
+    return this.collection.length;
+  },
 
-/**
- * Создание коллекции из массива значений
- */
-Collection.from = function () {};
+  at: function(position) {
+    if (position <= 0 || position > this.collection.length) {
+      // may be >=
+      return null;
+    } else {
+      return this.collection[position - 1];
+    }
+  },
+
+  removeAt: function(positionClear) {
+    if (
+      positionClear <= 0 ||
+      positionClear > this.collection.length ||
+      undefined
+    ) {
+      return false;
+    } else {
+      this.collection.splice(positionClear - 1, 1);
+      return true;
+    }
+  }
+};
+
+Collection.prototype = Object.create(methods);
+
+Collection.from = function(array) {
+  let collection = new Collection(array);
+  return collection;
+};
