b89e9ce7e6c174ce07c55223a8bd4ff62d5dcfe8
Ivan <ivaneroshk@yandex.ru>
2018-08-07 02:25:11 +0300

    Parallel execution of asynchronous functions

--- w8/exercise1/index.js
+++ w8/exercise1/index.js
@@ -2,6 +2,25 @@
  * @param {Function[]} operations
  * @param {Function} callback
  */
-module.exports = function (operations, callback) {
+module.exports = function(operations, callback) {
+  const promises = operations.map(operation => {
+    return new Promise((resolve, reject) => {
+      operation((error, result) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(result);
+      });
+    });
+  });
 
+  Promise.all(promises).then(
+    results => {
+      callback(null, results);
+    },
+    error => {
+      callback(error, null);
+    }
+  );
 };
